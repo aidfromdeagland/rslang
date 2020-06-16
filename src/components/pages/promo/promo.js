17d617fc64c201028f0171b1dcf66ef5f3f9f46c
aidfromdeagland <aidfromdeagland@gmail.com>
2020-06-16 13:02:21 +0300

    init: initial raw layout for promo

--- src/components/pages/promo/promo.js
+++ src/components/pages/promo/promo.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './promo.scss';
 
 export class Promo extends Component {
-  render() {
-    return (
-      <div className="promo">
-        <h1>Promo</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="promo">
+                <h1 className="promo__heading">iKnow</h1>
+                <section className="promo__about">
+                    <h2>О чем речь?</h2>
+                </section>
+                <section className="promo__advantages">
+                    <h2>Преимущества</h2>
+                </section>
+                <section className="promo__feedback">
+                    <h2>Пользователи о нас</h2>
+                </section>
+            </div>
+        );
+    }
 }
