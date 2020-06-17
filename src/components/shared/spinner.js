c114563a1b22703eb08f6bc53be2c66d5114b400
Artem <artem.antonov1993@gmail.com>
2020-06-17 19:41:34 +0300

    feat: add spinner

---
+++ src/components/shared/spinner.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './spinner.scss';
+
+export class Spinner extends Component {
+  render() {
+    return (
+        <div class="icon-spinner-5 spinner"></div>
+    );
+  }
+}
