7a811e027d77795f54a3ff145afe057bc64a16ee
Artem <artem.antonov1993@gmail.com>
2020-06-17 21:22:22 +0300

    refactor: fix the mistakes and add font locally

--- src/components/shared/spinner.js
+++ src/components/shared/spinner.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './spinner.scss';
 
 export class Spinner extends Component {
-  render() {
-    return (
-        <div class="icon-spinner-5 spinner"></div>
-    );
-  }
+    render() {
+        return (
+            <div className="spinner"></div>
+        );
+    }
 }
