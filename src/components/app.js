cb07dee2a00d4071717207b2169c4486de745284
Yulia Shchuklina <scrambler88@mail.ru>
2020-06-18 21:47:08 +0300

    feat: add component Nav

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './app.scss';
 import {
-  BrowserRouter as Router, Route, Switch,
+    BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
 import { Nav } from './nav/nav';
 import { AboutTeam } from './pages/aboutTeam/aboutTeam';
@@ -15,58 +15,58 @@ import { Vocabulary } from './pages/vocabulary/vocabulary';
 import { NotFound } from './pages/notFound/notFound';
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
 
-  render() {
-    const { text } = this.props;
-    return (
-      <Router>
-        <div className="App">
-          <header>
-            <Nav />
-            <p>{text}</p>
-          </header>
-          <main>
-            <Switch>
-              <Route path="/auth">
-                <Auth />
-              </Route>
-              <Route path="/mini-games">
-                <MiniGames />
-              </Route>
-              <Route path="/main">
-                <Main />
-              </Route>
-              <Route path="/stats">
-                <Stats />
-              </Route>
-              <Route path="/vocabulary">
-                <Vocabulary />
-              </Route>
-              <Route path="/about-team">
-                <AboutTeam />
-              </Route>
-              <Route exact path="/">
-                <Promo />
-              </Route>
-              <Route path="/*">
-                <NotFound />
-              </Route>
-            </Switch>
-          </main>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        const { text } = this.props;
+        return (
+            <Router>
+                <div className="App">
+                    <header>
+                        <Nav />
+                        <p>{text}</p>
+                    </header>
+                    <main>
+                        <Switch>
+                            <Route path="/auth">
+                                <Auth />
+                            </Route>
+                            <Route path="/mini-games">
+                                <MiniGames />
+                            </Route>
+                            <Route path="/main">
+                                <Main />
+                            </Route>
+                            <Route path="/stats">
+                                <Stats />
+                            </Route>
+                            <Route path="/vocabulary">
+                                <Vocabulary />
+                            </Route>
+                            <Route path="/about-team">
+                                <AboutTeam />
+                            </Route>
+                            <Route exact path="/">
+                                <Promo />
+                            </Route>
+                            <Route path="/*">
+                                <NotFound />
+                            </Route>
+                        </Switch>
+                    </main>
+                </div>
+            </Router>
+        );
+    }
 }
 
 App.defaultProps = {
-  text: '',
+    text: '',
 };
 
 App.propTypes = {
-  text: PropTypes.string,
+    text: PropTypes.string,
 };
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,33 +3,43 @@ import { Link } from 'react-router-dom';
 import './nav.scss';
 
 export class Nav extends Component {
-  render() {
-    return (
-      <nav className="navigation">
-        <ul className="navigation__list">
-          <li>
-            <Link to="/">Promo</Link>
-          </li>
-          <li>
-            <Link to="/main">Main</Link>
-          </li>
-          <li>
-            <Link to="/vocabulary">Vocabulary</Link>
-          </li>
-          <li>
-            <Link to="/stats">Statistic</Link>
-          </li>
-          <li>
-            <Link to="/mini-games">Mini games</Link>
-          </li>
-          <li>
-            <Link to="/auth">Auth</Link>
-          </li>
-          <li>
-            <Link to="/about-team">About team</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav className="navigation">
+                <ul className="navigation__list navigation__list-showed">
+                    <li>
+                        <div className="navigation__icon main" />
+                        <Link to="/main">Main</Link>
+                    </li>
+
+                    <li>
+                        <div className="navigation__icon vocabulary" />
+                        <Link to="/vocabulary">Vocabulary</Link>
+                    </li>
+                    <li>
+                        <div className="navigation__icon minigames" />
+                        <Link to="/mini-games">Mini games</Link>
+                    </li>
+                    <li>
+                        <div className="navigation__icon statistics" />
+                        <Link to="/stats">Statistic</Link>
+                    </li>
+                    <li>
+                        <div className="navigation__icon promo" />
+                        <Link to="/">Promo</Link>
+                    </li>
+                    <li>
+                        <div className="navigation__icon aboutteam" />
+                        <Link to="/about-team">About team</Link>
+                    </li>
+
+                    <li>
+                        <div className="navigation__icon logout" />
+                        <Link to="/auth">Logout</Link>
+                    </li>
+
+                </ul>
+            </nav>
+        );
+    }
 }
