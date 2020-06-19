cd920efe5cb1c33e0a35555c48120419bb38213b
Yulia Shchuklina <scrambler88@mail.ru>
2020-06-19 23:15:04 +0300

    feat: add footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './footer.scss';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                &copy;iKnow 2020
+            </div>
+        );
+    }
+}
