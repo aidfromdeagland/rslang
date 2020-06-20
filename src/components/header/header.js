41fc4d21433290fb24487064194b0cb6fc222dc5
Yulia Shchuklina <scrambler88@mail.ru>
2020-06-21 00:05:00 +0300

    feat: add open/close to hamberger button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,17 +3,24 @@ import { Nav } from '../nav/nav';
 import './header.scss';
 
 export class Header extends Component {
+    state = {
+        isOpen: false,
+          };
+
     render() {
+        const { isOpen } = this.state;
         return (
             <header className="header">
-                <Nav />
+                <Nav isOpen={this.state.isOpen} />
                 <div className="header__container">
-                    <div className="header__navigation">
-                        <div className="header__hamburger">
-                            <div className="header__hamburger-open" />
+                                           <div 
+                        className="header__hamburger"
+                        onClick={() => this.setState({ isOpen: !isOpen })}
+                        >
+                            <div className={isOpen ? 'header__hamburger-close': 'header__hamburger-open'}/>
                         </div>
                         <div className="header__logo" />
-                    </div>
+                  
                     <div className="header__logout">
                         Logout
                     </div>
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,43 +4,44 @@ import './nav.scss';
 
 export class Nav extends Component {
     render() {
+        const { isOpen } = this.props;
         return (
             <nav className="navigation">
-                <ul className="navigation__list navigation__list-showed">
+                <ul className={isOpen ? 'navigation__list navigation__list-showed' : 'navigation__list'}>
                     <li>
                         <Link to="/main">
-                            <div className="navigation__icon main" />
+                            <div className="navigation__icon navigation__icon_main" />
                             <span>Main</span>
                         </Link>
                     </li>
 
                     <li>
                         <Link to="/vocabulary">
-                            <div className="navigation__icon vocabulary" />
+                            <div className="navigation__icon navigation__icon_vocabulary" />
                             <span>Vocabulary</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/mini-games">
-                            <div className="navigation__icon minigames" />
+                            <div className="navigation__icon navigation__icon_minigames" />
                             <span>Mini games</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/stats">
-                            <div className="navigation__icon statistics" />
+                            <div className="navigation__icon navigation__icon_statistics" />
                             <span>Statistic</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/">
-                            <div className="navigation__icon promo" />
+                            <div className="navigation__icon navigation__icon_promo" />
                             <span>Promo</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/about-team">
-                            <div className="navigation__icon aboutteam" />
+                            <div className="navigation__icon navigation__icon_aboutteam" />
                             <span>About team</span>
                         </Link>
                     </li>
