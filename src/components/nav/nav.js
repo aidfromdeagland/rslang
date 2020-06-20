6f2e2063ed9bf4b6a8d9c13ebb3079aaf14fd189
Yulia Shchuklina <scrambler88@mail.ru>
2020-06-20 23:32:54 +0300

    feat: add styles to ui and icons

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -9,38 +9,38 @@ export class Nav extends Component {
                 <ul className="navigation__list navigation__list-showed">
                     <li>
                         <Link to="/main">
-                            <div className="navigation__icon main" />
+                            <div className="navigation__icon navigation__icon_main" />
                             <span>Main</span>
                         </Link>
                     </li>
 
                     <li>
                         <Link to="/vocabulary">
-                            <div className="navigation__icon vocabulary" />
+                            <div className="navigation__icon navigation__icon_vocabulary" />
                             <span>Vocabulary</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/mini-games">
-                            <div className="navigation__icon minigames" />
+                            <div className="navigation__icon navigation__icon_minigames" />
                             <span>Mini games</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/stats">
-                            <div className="navigation__icon statistics" />
+                            <div className="navigation__icon navigation__icon_statistics" />
                             <span>Statistic</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/">
-                            <div className="navigation__icon promo" />
+                            <div className="navigation__icon navigation__icon_promo" />
                             <span>Promo</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/about-team">
-                            <div className="navigation__icon aboutteam" />
+                            <div className="navigation__icon navigation__icon_aboutteam" />
                             <span>About team</span>
                         </Link>
                     </li>
