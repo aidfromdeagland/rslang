090fc9e44d6960dd0b5f21dc6c33081b153ad172
dubkooleg@gmail.com <dubkooleg@gmail.com>
2020-06-21 17:17:31 +0300

    fix: remove start tests

--- src/components/pages/auth/auth.js
+++ src/components/pages/auth/auth.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import './auth.scss';
-import { wordServiceTest, settingServiceTest, statisticServiceTest } from '../../../services/wordServicesTest';
 
 export class Auth extends Component {
     constructor() {
         super();
-        // wordServiceTest();
-        settingServiceTest();
-        // statisticServiceTest();
     }
 
     render() {
