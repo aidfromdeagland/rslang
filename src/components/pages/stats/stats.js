5b415bd4fb64bf533f52442754d905329aa51ead
aidfromdeagland <aidfromdeagland@gmail.com>
2020-06-25 00:28:19 +0300

    fix: fix according to reviewers comments

--- src/components/pages/stats/stats.js
+++ src/components/pages/stats/stats.js
@@ -4,8 +4,8 @@ import './stats.scss';
 export class Stats extends Component {
     render() {
         return (
-            <div className="statistic">
-                <h1>Statistic</h1>
+            <div className="statistics">
+                <h1>Statistics</h1>
             </div>
         );
     }
