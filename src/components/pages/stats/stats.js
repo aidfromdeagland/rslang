7e7af5ed56c315cea4f4936ecc830ea5f94d0b24
aidfromdeagland <aidfromdeagland@gmail.com>
2020-06-24 11:54:12 +0300

    fix: fixed header, global box-sizing, style fixes

--- src/components/pages/stats/stats.js
+++ src/components/pages/stats/stats.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './stats.scss';
 
 export class Stats extends Component {
-  render() {
-    return (
-      <div className="promo">
-        <h1>Statistic</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="statistic">
+                <h1>Statistic</h1>
+            </div>
+        );
+    }
 }
