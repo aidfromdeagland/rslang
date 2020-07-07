f5a51453cb8989ee1ffdcc2523ed5edf6167bc3b
aidfromdeagland <aidfromdeagland@gmail.com>
2020-07-07 10:51:51 +0300

    fix: change default settings template

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -5,14 +5,20 @@ export const GROUP_COUNT = 6;
 export const PAGE_COUNT = 30;
 export const MEDIA_PREFIX_URL = 'https://raw.githubusercontent.com/aidfromdeagland/rslang-data/master/';
 export const settingsDefault = {
+    wordsPerDay: 20,
     optional: {
-        word: true,
-        textMeaning: true,
-        textExample: true,
-        showPicture: true,
-        showTranscription: true,
-        numberLearnWord: 10,
-        numberLearnCard: 10,
+        totalWords: 20,
+        newWords: 10,
+        showWordTranslate: true,
+        showSentenceMeaning: true,
+        showSentenceExample: true,
+        showWordTranscription: true,
+        showWordImage: true,
+        showSentencesTranslate: true,
+        showAnswerButton: true,
+        showDeleteButton: true,
+        showDifficultButton: true,
+        autoPronunciation: true,
         audioCall: '{modeGame:1,"wordCount":10,"colorStart":{"r":9,"g":44,"b":112},"colorEnd":{"r":224,"g":141,"b":157},"group":0,"page":0}',
     },
 };
