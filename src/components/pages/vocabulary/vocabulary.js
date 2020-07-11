f157ef65e3799f76e6211d148cc77b6f4521637c
aidfromdeagland <aidfromdeagland@gmail.com>
2020-07-11 12:48:01 +0300

    feat: bind  vocabulary to backend

--- src/components/pages/vocabulary/vocabulary.js
+++ src/components/pages/vocabulary/vocabulary.js
@@ -11,6 +11,7 @@ import { wordsMockLearned, wordsMockDifficult, wordsMockDeleted } from './tempMo
 import { WordList } from './wordList';
 import { WordService } from '../../../services/wordServices';
 import { Spinner } from '../../shared/spinner';
+import { SettingService } from '../../../services/settingServices';
 
 const maximumWordsQuantity = 3600;
 
@@ -18,8 +19,17 @@ export class Vocabulary extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isWordsLoaded: false,
+            isDataReady: false,
         };
+    }
+
+    componentDidMount() {
+        this.getReadyData();
+    }
+
+    getReadyData = async () => {
+        await Promise.all([this.getWords(), this.getSettings()]);
+        this.setState({ isDataReady: true });
     }
 
     getWords = async () => {
@@ -28,16 +38,25 @@ export class Vocabulary extends Component {
         this.difficultWords = [];
         this.deletedWords = [];
 
-        const totalWords = await WordService.getUserAggWords(
+        const totalWordsResponse = await WordService.getUserAggWords(
             '', totalUserWordsAggregatedQuery, maximumWordsQuantity,
         );
-        console.log(totalWords);
+        const totalWords = totalWordsResponse[0].paginatedResults;
+        this.learnedWords = totalWords.filter((wordObject) => wordObject.userWord.optional.isDeleted === false && wordObject.userWord.optional.isDifficult === false);
+        this.difficultWords = totalWords.filter((wordObject) => wordObject.userWord.optional.isDifficult === true);
+        this.deletedWords = totalWords.filter((wordObject) => wordObject.userWord.optional.isDeleted === true);
+        console.log('WORDS: ', this.learnedWords, this.difficultWords, this.deletedWords);
     };
 
+    getSettings = async () => {
+        const settings = await SettingService.get();
+        this.settings = settings.optional;
+        console.log('SETTINGS: ', this.settings);
+    }
+
     render() {
-        const { settings } = this.props;
-        const { isWordsLoaded } = this.state;
-        return isWordsLoaded
+        const { isDataReady } = this.state;
+        return isDataReady
             ? (
                 <div className="content-container">
                     <Tabs className="vocabulary" selectedTabClassName="vocabulary__tab_active" defaultFocus>
@@ -48,13 +67,13 @@ export class Vocabulary extends Component {
                         </TabList>
 
                         <TabPanel className="vocabulary__panel">
-                            <WordList words={wordsMockLearned} settings={settings} />
+                            <WordList words={this.learnedWords} settings={this.settings} />
                         </TabPanel>
                         <TabPanel className="vocabulary__panel">
-                            <WordList words={wordsMockDifficult} settings={settings} isSpecial />
+                            <WordList words={this.difficultWords} settings={this.settings} isSpecial />
                         </TabPanel>
                         <TabPanel className="vocabulary__panel">
-                            <WordList words={wordsMockDeleted} settings={settings} isSpecial />
+                            <WordList words={this.deletedWords} settings={this.settings} isSpecial />
                         </TabPanel>
                     </Tabs>
                 </div>
@@ -62,24 +81,3 @@ export class Vocabulary extends Component {
             : <Spinner />;
     }
 }
-
-Vocabulary.defaultProps = {
-    settings: {
-        mainSettings: {
-            word: true,
-            sentence: true,
-            textMeaning: true,
-        },
-        additionalSettings: {
-            transcription: true,
-            image: true,
-        },
-    },
-};
-
-Vocabulary.propTypes = {
-    settings: PropTypes.objectOf(PropTypes.shape({
-        mainSettings: PropTypes.object,
-        additionalSettings: PropTypes.object,
-    })),
-};
