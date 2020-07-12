44f3e68feedc55e4c04f0a8e42a0219a428f871d
aidfromdeagland <aidfromdeagland@gmail.com>
2020-07-12 13:45:53 +0300

    fix: edit personal info

--- src/components/pages/aboutTeam/dataAboutTeam.js
+++ src/components/pages/aboutTeam/dataAboutTeam.js
@@ -1,42 +1,48 @@
-import avatar from "../../../assets/images/avatars/avatar.png";
+import avatar from '../../../assets/images/avatars/avatar.png';
 
 const dataAboutTeam = [
     {
         photo: avatar,
         name: 'Gleb',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        description: 'React promoter:), mentoring, reviewing, initial organization questions.',
+        gitHub: '#',
     },
     {
         photo: avatar,
-        name: 'Stanislav',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        name: 'Stas',
+        description: 'Reviewing, environment setting, promo page, vocabulary page, mini game "Speak it", main application logic, other minor features.',
+        gitHub: 'https://github.com/aidfromdeagland',
     },
     {
         photo: avatar,
         name: 'Natalia',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        description: 'Design, content graphic, mini game "Sprint", other minor features.',
+        gitHub: 'https://github.com/natein',
     },
     {
         photo: avatar,
         name: 'Oleg',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        description: 'All project services, error logger, authorisation page, mini game "Audio call",  other minor features',
+        gitHub: 'https://github.com/OlegD89',
     },
     {
         photo: avatar,
         name: 'Yulia',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        description: 'Design, statistics page, mini game "Savannah", navigation component, other minor features.',
+        gitHub: 'https://github.com/mrJozhkinKot',
     },
     {
         photo: avatar,
         name: 'Artem',
-        description: 'Here you will find information about you and your contribution to this project.',
-        gitHub: '#'
+        description: 'Main page, study page, about team page, mini games page, mini game "English puzzle", mini game "Hangman", mini game "Speak it", other minor features.',
+        gitHub: 'https://github.com/Dirtymalka',
     },
-]
+    {
+        photo: avatar,
+        name: 'Olga',
+        description: 'mini game "Hangman", other minor features',
+        gitHub: 'https://github.com/HelgaAnt',
+    },
+];
 
 export default dataAboutTeam;
