f026f37ef234a67fed20541db1bcf063be2c6d62
Artem <artem.antonov1993@gmail.com>
2020-07-13 19:25:41 +0300

    feat: add folder hangman

---
+++ src/components/games/hangman/hangman.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './hangman.scss';
+
+export class Hangman extends Component {
+    render() {
+        return (
+            <div className="hangman">
+            </div>
+        );
+    }
+}
